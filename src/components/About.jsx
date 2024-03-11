f96c8d0e8c568e6fb0349abd8e3ab313810e75a6
summit3 <summitkhadka3@gmail.com>
2024-03-11 11:48:10 +0000

    Add Animated Blob

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,9 @@
-import spacer from "../../public/Svg/layered-waves-haikei.svg"
-import Image from "next/image"
+import spacer1 from "../../public/Svg/layered-waves-haikei.svg";
+import spacer2 from "../../public/Svg/layered-waves-haikei-2.svg";
+import Image from "next/image";
 
 export default function About() {
-  return <section className="h-screen bg-secondary">
-    <Image className="spacer" src={spacer}></Image>
-    <h1>About</h1>
-    <Image className="spacer rotate-180" src={spacer}></Image>
-  </section>
+  return (
+    <section className="h-screen bg-secondary grid grid-cols-8 grid-rows-2"></section>
+  );
 }
