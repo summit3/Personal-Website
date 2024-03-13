3c96c30652870891ad12b3a0b6339281df32523d
summit3 <summitkhadka3@gmail.com>
2024-03-13 12:09:03 +0000

    FInish About Page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,11 @@ import Grid from "./About/Grid";
 
 export default function About() {
   return (
-    <section className="h-screen bg-secondary text-black text-3xl flex flex-col justify-center items-center">
-      <h2 className="font-bold text-primary block">About</h2>
+    <section className="h-screen bg-secondary text-black text-3xl flex flex-col items-center">
+      <hr className="w-screen border-b-2 border-neutral-300 mb-3"></hr>
+      <h2 className="w-screen font-bold text-primary text-center my-10">
+        About
+      </h2>
       <Grid />
     </section>
   );
