6595ad421a5ab835790eac00adb16e8259a0a0bd
summit3 <summitkhadka3@gmail.com>
2024-03-20 08:44:17 +0000

    Begin Working On Projects Page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,10 @@
 export default function Projects() {
-  return <section className="h-screen bg-third">
-    <h1>Projects</h1>
-  </section>
+  return (
+    <section className="h-screen bg-secondary">
+      <hr className="w-screen border-b-2 border-neutral-300 mb-3"></hr>
+      <h2 className="w-screen font-bold text-primary text-center my-10">
+        Projects
+      </h2>
+    </section>
+  );
 }
