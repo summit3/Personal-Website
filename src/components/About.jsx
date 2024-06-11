bf3e1ec0269a1752a4412a7949058f5492ebdb6b
summit3 <summitkhadka3@gmail.com>
2024-06-11 08:49:43 +0100

    Filling in About Page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,50 @@
-import Grid from "./About/Grid";
+import Image from "next/image";
+import code from "../../public/Icons/code-square.svg";
+import excel from "../../public/Icons/pngwing.com.png";
+import python from "../../public/Icons/python.png";
 
 export default function About() {
   return (
     <section
-      className="scroll-smooth h-screen bg-secondary text-black text-3xl flex flex-col items-center"
+      className="scroll-smooth bg-secondary text-black text-3xl flex flex-col items-center"
       id="about"
     >
       <hr className="w-screen border-b-2 border-neutral-300 mb-3 mt-24"></hr>
       <h2 className="w-screen font-bold text-primary text-center mt-10 mb-16">
         About
       </h2>
-      <Grid />
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 w-screen justify-items-center">
+        <div className="flex flex-col justify-center items-center">
+          <div className="bg-primary p-3 flex items-center justify-center rounded-lg h-24 w-24">
+            <Image className="col-start-1 h-12 w-12 invert" src={code} />
+          </div>
+          <div className="px-16 py-7 text-center text-md">
+            Experienced in web development, utilizing frameworks such as React
+            and Tailwind CSS to create responsive and visually appealing
+            websites. Find my work on the projects page.
+          </div>
+        </div>
+        <div className="flex flex-col justify-center items-center">
+          <div className="bg-primary p-3 flex items-center justify-center rounded-lg h-24 w-24">
+            <Image className="col-start-2 h-12 w-12 invert" src={excel} />
+          </div>
+          <div className="px-16 py-7 text-center">
+            Skilled in developing comprehensive and dynamic spreadsheets using
+            both Excel and Google Sheets, enabling efficient data analysis,
+            organization, and presentation.
+          </div>
+        </div>
+        <div className="flex flex-col justify-center items-center">
+          <div className="bg-primary p-3 flex items-center justify-center rounded-lg h-24 w-24">
+            <Image className="col-start-3 h-12 w-12 invert" src={python} />
+          </div>
+          <div className="px-16 py-7 text-center">
+            Fluent in Python for data analysis using libraries such as
+            Scikit-Learn, Pandas, and Matplotlib to execute machine learning
+            algorithms and perform complex data modeling and analysis.
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
--- src/components/Projects/Card.jsx
+++ src/components/Projects/Card.jsx
@@ -1,6 +1,6 @@
 export default function Card() {
   return (
-    <div className="bg-white grid grid-cols-1 w-96 rounded-md border border-black border-1 overflow-hidden">
+    <div className="bg-white grid grid-cols-1 w-96 rounded-md border border-black border-1 overflow-hidden card">
       <div className="bg-primary h-64"></div>
       <div className="h-auto text-sm px-2 py-3 text-center">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec purus
