98b967a7d7495870160881b976fb461b1208cf10
summit3 <summitkhadka3@gmail.com>
2024-06-11 13:06:58 +0100

    Replacing all apostrophes

--- src/components/Landing/Title.jsx
+++ src/components/Landing/Title.jsx
@@ -1,7 +1,7 @@
 export default function Title() {
   return (
     <div className="row-start-2 col-start-2 ml-5">
-      <p className="text-black text-3xl my-3">Hi, I'm</p>
+      <p className="text-black text-3xl my-3">Hi, I&apos;m</p>
       <p className="text-primary font-bold text-5xl my-3">Summit Khadka</p>
       <p className="text-black text-3xl my-3">
         I am a <span className="text-primary font-bold">Programmer</span>
